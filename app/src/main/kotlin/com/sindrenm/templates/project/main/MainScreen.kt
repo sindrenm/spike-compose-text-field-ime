31cc7505d6d8faccdd8ade5cee5f68539f0cab37
Sindre Moen <sindrem@gmail.com>
2023-09-14 17:04:51 +0200

    Bring in app bar and next-item focusing

--- app/src/main/kotlin/com/sindrenm/templates/project/main/MainScreen.kt
+++ app/src/main/kotlin/com/sindrenm/templates/project/main/MainScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+
 package com.sindrenm.templates.project.main
 
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -9,34 +11,34 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @Composable
 internal fun MainScreen() {
   Scaffold(
-    topBar = {
-      Spacer(
-        Modifier
-          .statusBarsPadding()
-          .padding(top = 12.dp),
-      )
-    },
+    topBar = { TopAppBar(title = { Text("Top App Bar") }) },
     bottomBar = {
       Spacer(
         Modifier
@@ -65,6 +67,7 @@ fun YourTextField(index: Int) {
   val (value, onValueChange) = remember { mutableStateOf("") }
   val bringIntoViewRequester = remember { BringIntoViewRequester() }
   val coroutineScope = rememberCoroutineScope()
+  val focusManager = LocalFocusManager.current
 
   TextField(
     value = value,
@@ -81,5 +84,10 @@ fun YourTextField(index: Int) {
           }
         }
       },
+    singleLine = true,
+    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+    keyboardActions = KeyboardActions(
+      onNext = { focusManager.moveFocus(FocusDirection.Next) }
+    )
   )
 }
